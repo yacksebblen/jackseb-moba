459e4bc86d113a62fcac7f7601ac6de5541df5cd
yacksebblen <johnjacksebben@gmail.com>
2020-06-26 16:12:05 -0500

    Player movement + indicator, camera follow

---
+++ Jackseb MOBA/Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public Transform indicator;
+
+	NavMeshAgent myAgent;
+
+	void Start()
+    {
+		myAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+		{
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+			{
+				myAgent.SetDestination(hit.point);
+
+				indicator.position = hit.point + hit.normal * 0.1f;
+			}
+		}
+    }
+}
